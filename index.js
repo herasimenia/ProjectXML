f839e8a38eba9030e464ef20dfef1c6c3ac4f7a9
Aleh Herasimenia <herasimenia@gmail.com>
2020-11-06 03:25:13 +0300

    Изменил приветствие при старте

--- index.js
+++ index.js
@@ -16,57 +16,57 @@ const Gzip = zlib.createGzip({
 });
 
 function ActualBears() {
-    Transform.call(this)
+	Transform.call(this)
 }
 
 inherits(ActualBears, Transform)
 
-ActualBears.prototype._transform = function(chunk, enc, done) {
+ActualBears.prototype._transform = function (chunk, enc, done) {
 
-    chunk = minifyXML(chunk
-        .toString()
-        .replace(/ +/g, ' ')
-        .replace(/ ;+/g, ';')
-        .replace(/(<!DOCTYPE html)(.*\n)+^.+/gim, '')
-    )
+	chunk = minifyXML(chunk
+		.toString()
+		.replace(/ +/g, ' ')
+		.replace(/ ;+/g, ';')
+		.replace(/(<!DOCTYPE html)(.*\n)+^.+/gim, '')
+	)
 
-    this.push(chunk)
-    done()
+	this.push(chunk)
+	done()
 }
 
 var download = request('https://api.21vek.by/feeds/115587.xml', 'utf8')
 var write = fs.createWriteStream(__dirname + '/src/115587.min.xml.gz', 'utf8')
 
 download
-    .pipe(new ActualBears())
-    .pipe(through2(
-        (chunk, enc, cb) => cb(null, chunk), // transform is a noop
-        function(cb) { // flush function
+	.pipe(new ActualBears())
+	.pipe(through2(
+		(chunk, enc, cb) => cb(null, chunk), // transform is a noop
+		function (cb) { // flush function
 
-            this.push('</offer></offers></shop></yml_catalog>');
-            cb();
-        }
-	 ))
-	 .pipe(Gzip)
-	 .pipe(write)
-	 
-    .on('finish', () => {
+			this.push('</offer></offers></shop></yml_catalog>');
+			cb();
+		}
+	))
+	.pipe(Gzip)
+	.pipe(write)
 
-        figlet.text("XML end!", function(error, data) {
-            if (error)
-                console.error(error);
-            else
-                console.log(data);
-            console.log('Обработка файла закончена.');
-        });
+	.on('finish', () => {
 
-	 })
-	 
+		figlet.text("XML end!", function (error, data) {
+			if (error)
+				console.error(error);
+			else
+				console.log(data);
+			console.log('Обработка файла закончена.');
+		});
 
-figlet.text("XML start!", function(error, data) {
-    if (error)
-        console.error(error);
-    else
-        console.log(data);
-    console.log('Обработка файла началась.');
+	})
+
+
+figlet.text("XML start! Wait, please.", function (error, data) {
+	if (error)
+		console.error(error);
+	else
+		console.log(data);
+	console.log('Обработка файла началась.');
 });
